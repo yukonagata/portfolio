2242222fac6a7ba2dffd11b4e9630bf83bb17d22
yuko <81068306+yukonagata@users.noreply.github.com>
2021-04-24 21:46:08 +0900

    yuko

---
+++ YukoPress/js/script.js
@@ -0,0 +1,66 @@
+'use strict';
+
+console.log('読み込めてるよ！');
+
+//------------------フェードインアニメーション------------------------
+AOS.init();
+//------------------フェードインアニメーションここまで------------------------
+
+//------------------トップに戻るボタン------------------------
+window.onscroll = () => {
+    let position = document.documentElement.scrollTop || document.body.scrollTop;
+    const pageTop = document.getElementById('topBtn');
+    if (position >= 350) {
+        pageTop.classList.add('active');
+    } else {
+        pageTop.classList.remove('active');
+    }
+};
+//------------------トップに戻るボタンここまで----------------
+
+
+
+//------------------ハンバーガーメニュー----------------------
+const gNaviBtn = document.getElementById('gNaviBtn');
+const gNavi = document.getElementById('gNavi');
+
+gNaviBtn.onclick = (e) => {
+    e.preventDefault;
+    gNaviBtn.classList.toggle('active');
+    gNavi.classList.toggle('active');
+};
+
+gNavi.onclick = (e) => {
+    e.preventDefault;
+    gNaviBtn.classList.remove('active');
+    gNavi.classList.remove('active');
+};
+//------------------ハンバーガーメニューここまで--------------
+
+
+//------------------好きなことSwiper-------------------------
+    var swiper = new Swiper('.swiper-container', {
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+        renderBullet: function (index, className) {
+            //中に数字を表示
+            return '<span class="' + className + '">' + (index + 1) + '</span>';
+        }
+        },
+    
+      slidesPerView: 1,
+      spaceBetween: 30,
+      loop: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      effect: "fade",
+      fadeEffect: {
+        crossFade: true // クロスフェードさせる
+      },
+    }); 
+//------------------好きなことSwiperここまで------------------
+
