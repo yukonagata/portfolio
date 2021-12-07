7d8b8d9a6e6573c9def16ba22518c776e6c6d3fd
yuko <81068306+yukonagata@users.noreply.github.com>
2021-12-07 18:56:32 +0900

    noza

---
+++ noza/script.js
@@ -0,0 +1,44 @@
+'use strict';
+
+console.log('読み込めてるよ！');
+
+//------------------トップに戻るボタン------------------------
+window.onscroll = () => {
+    const position = document.documentElement.scrollTop || document.body.scrollTop;
+    const pageTop = document.getElementById('topBtn');
+    if (position >= 600) {
+        pageTop.classList.add('active');
+    } else {
+        pageTop.classList.remove('active');
+    }
+
+    const pageTop2 = document.getElementById('header');
+    if (position >= 600) {
+        pageTop2.classList.add('active');
+    } else {
+        pageTop2.classList.remove('active');
+    }
+
+};
+//------------------トップに戻るボタンここまで----------------
+
+
+//------------------アコーディオンメニュー------------------------
+jQuery(function ($) {  
+    // 最初のコンテンツ以外は非表示
+    $(".qanda__box:not(:first-of-type) dd").css("display", "none");
+    // 矢印の向きを変えておく
+    $(".qanda__box:first-of-type dt").addClass("open");
+    //タイトルがクリックされたら
+    $(".qanda__q").on('click', function () {
+      //クリックしたjs-accordion-title以外の全てのopenを取る
+      $(".qanda__q").not(this).removeClass("open");
+      //クリックされたjs-accordion-title以外のcontentを閉じる
+      $(".qanda__q").not(this).next().slideUp(300);
+      //thisにopenクラスを付与
+      $(this).toggleClass("open");
+      //thisのcontentを展開、開いていれば閉じる
+      $(this).next().slideToggle(300);
+    });
+  });
+//------------------アコーディオンメニューここまで----------------
